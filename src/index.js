1793989bb4d519f5d0df774a2a3b99190cb67d67
TaniaGJimenez <122074803+TaniaGJimenez@users.noreply.github.com>
2023-02-01 22:05:59 -0600

    Modificaciones con CSS.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 import validator from './validator.js';
 
+//AddEventListener para el botón
+const Validacion = document.getElementById("BtnValidar");
+Validacion.addEventListener("click", validate)
+
+//Función para los mensajes de Tarjeta Valida, Tarjeta Invalida
+function validate() {
+  const cardNumber = document.getElementById("cardnumber").value
+  
+  console.log(cardNumber)  
+  //If/Else statments para llenar formulario, saber si la tarjeta es valida    
+  if (cardNumber ===""){
+    alert("Ingresa tu numero de tarjeta")
+    return
+  }
+  
+  //Llamando al método en el contexto del objeto
+  const valid = validator.isValid(cardNumber)
+  if(valid===true) {
+    document.getElementById('result').innerHTML=(
+      `Numero de Tarjeta ${validator.maskify(cardNumber)} Valido. Puede continuar su compra`);
+  }
+  else {document.getElementById('result').innerHTML=(
+    `Numero de Tarjeta ${validator.maskify(cardNumber)} no existe. Favor de Verificar`);
+  }
+}
 console.log(validator);
+
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,57 @@
 const validator = {
-  // ...
-};
+  /*Definición
+   de Método: Función dentro de un objeto*/
+  isValid:(creditCardNumber)=> {
+    //Convertir el input en Array usando Array.From()
+    const arr = Array.from((creditCardNumber))
+    console.log(Array.from(creditCardNumber), "Original")
+    //Invertir el Array usando la nueva constante usando .Reverse()
+    arr.reverse();  
+    console.log(arr, "Invertido");
+    //Convertir el array Invertido en Numeros con .map.
+    const nums = arr.map(numStr=>parseInt(numStr));
+    console.log(nums,"Array de Nùmeros")
+    //La variable sum que contiene la suma de los valores en el array
+    let sum = 0;
+    //For que recorre todo mi array
+    for (let i = 0; i < nums.length; i++) {
+      if (i % 2 === 0) {
+        const numG1 = (nums[i]);
+        sum+= numG1;
+      } else {
+        //Aqui multiplico los pares.
+        const numG2 = (nums[i] * 2);
+        // "Nested conditional" por si el num es mayor o igual a diez, restar nueve.
+        if (numG2 >= 10) {
+          sum += numG2 - 9;
+        } else {
+          sum += numG2;
+        }
+      }
+    }
+    //Si el residuo de la suma es igual a cero entonces sum es True/sino False
+    sum = sum % 10 === 0 
+  
+    return sum
+  
+  },
+
+  maskify:(creditCardNumber)=> {
+    let MaskNum = [];
+    for (let i = 0; i < creditCardNumber.length; i++) {
+      if (i < creditCardNumber.length - 4) {
+        //MaskNum.push("#");
+        const Kitty = ("#");
+        MaskNum += Kitty;
+      } else {
+        //MaskNum.push(creditCardNumber[i]);
+        const UnHide = (creditCardNumber[i]);
+        MaskNum +=UnHide
+      }
+    }
+    return MaskNum;
+  }
+}
+console.log(validator.maskify("1234567812345678"))
 
 export default validator;
